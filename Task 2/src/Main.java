6ded2c46276dcb04d3f49fb0059fccf41d72fac8
SabyrrZhaslanuly <sabyrr.zhaslanuly@gmail.com>
2025-02-19 14:15:41 +0500

    hw 9 was completed

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        isEvenOrOdd();
+    }
+    public static void isEvenOrOdd() {
+        System.out.print("Введите число: ");
+        int userInputNumber = new Scanner(System.in).nextInt();
+
+        System.out.println(userInputNumber % 2 == 0);
     }
 }
